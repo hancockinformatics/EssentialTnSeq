03b7fa9baccbeb7c81a980d67959cb4da78e0083
Travis M Blimkie <travis.m.blimkie@gmail.com>
2020-07-22 15:43:10 -0700

    Small change to startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@
 
 .onAttach <- function(...) {
   packageStartupMessage(paste0(
-    "\nThanks for using EssentialTnSeq v", packageVersion("EssentialTnSeq"), "\n",
+    "Thanks for using EssentialTnSeq v", packageVersion("EssentialTnSeq"), "!\n",
     "If you encounter any bugs or problems, please submit an issue at the\n",
     "Github page: https://github.com/hancockinformatics/EssentialTnSeq/issues\n"
   ))
